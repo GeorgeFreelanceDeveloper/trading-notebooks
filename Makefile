d62c3da3a79cd7468222e2d6758d17529324919b
GeorgeQuantAnalyst <jiri.caga@proton.me>
2023-04-21 14:56:31 +0200

    Refactoring Bybit place trades pages part 1

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 # Signifies our desired python version
 # Makefile macros (or variables) are defined a little bit differently than traditional bash, keep in mind that in the Makefile there's top-level Makefile-only syntax, and everything else is bash script syntax.
-PYTHON = python3
+PYTHON = python3.11
 
 # .PHONY defines parts of the makefile that are not dependant on any specific file
 # This is most often used to store functions
--- notebooks/placeTrades/bybit/utils.py
+++ notebooks/placeTrades/bybit/utils.py
@@ -7,8 +7,6 @@ import decimal
 MIN_LEVERAGE = 1
 DEFAULT_PRICE_SCALE = 2
 DEFAULT_QTY_STEP = "1"
-ACCOUNT_TYPE = "bybitPositionAccount"
-
 
 def load_config(config_file: str) -> dict:
     with open(config_file, 'r') as stream:
@@ -18,28 +16,20 @@ def load_config(config_file: str) -> dict:
         except yaml.YAMLError as exc:
             print(exc)
 
-
-def create_account_info(wallet_balance_raw: dict, config: dict):
-    wallet_balance = wallet_balance_raw["result"]["USDT"]["available_balance"]
-    return {
-        "balanceOnExchange": round(wallet_balance, 2),
-        "balanceInBank": round(config[ACCOUNT_TYPE]["balanceInBank"], 2),
-        "accountSize": round(wallet_balance + config[ACCOUNT_TYPE]["balanceInBank"], 2)
-    }
-
-
-def transform_trades(trades_raw: pd.DataFrame, instruments_info: dict, config: dict) -> pd.DataFrame:
+def transform_trades(trades_raw: pd.DataFrame, instruments_info: dict, config: dict, account_type: str) -> pd.DataFrame:
     trades = trades_raw.copy()
 
-    risk_per_trade_usd = config[ACCOUNT_TYPE]["riskPerTradeUsd"]
-    futures_account_size_usd = config[ACCOUNT_TYPE]["futuresAccountSizeUsd"] * \
-        config[ACCOUNT_TYPE]["usedMarginAccount"]
+    risk_per_trade_usd = config[account_type]["riskPerTradeUsd"]
+    futures_account_size_usd = config[account_type]["futuresAccountSizeUsd"] * \
+        config[account_type]["usedMarginAccount"]
+    trigger_price_distance_percentage = config[account_type]["triggerPriceDistancePercentage"]
 
     trades["move"] = trades["entry_price"] - trades["stop_loss"]
     trades["profit_target"] = trades["entry_price"] + trades["move"]
     trades["position"] = abs(risk_per_trade_usd/trades["move"])
     trades["position_usd"] = trades["position"] * trades["entry_price"]
     trades["leverage"] = trades["position_usd"] / futures_account_size_usd
+    trades["trigger_price"] = trades["entry_price"] + (trades["move"] * trigger_price_distance_percentage)
 
     # Round leverage
     trades['leverage'] = trades['leverage'].astype(float)
@@ -53,6 +43,8 @@ def transform_trades(trades_raw: pd.DataFrame, instruments_info: dict, config: d
         instruments_info, row["ticker"]), axis=1)
     trades["entry_price"] = trades.apply(lambda x: round(
         x["entry_price"], x["price_scale"]), axis=1)
+    trades["trigger_price"] = trades.apply(lambda x: round(
+        x["trigger_price"], x["price_scale"]), axis=1)
     trades["profit_target"] = trades.apply(lambda x: round(
         x["profit_target"], x["price_scale"]), axis=1)
     trades["stop_loss"] = trades.apply(lambda x: round(
@@ -77,30 +69,28 @@ def place_trades_on_exchange(trades: pd.DataFrame, exchange) -> pd.DataFrame:
     response = []
 
     for index, trade in trades.iterrows():
-
         try:
-            __set_leverage(exchange, trade["ticker"], trade["leverage"])
-
             __set_isolate_margin(exchange,
                                  trade["ticker"],
                                  trade["leverage"],
                                  trade["leverage"])
+            
+            __set_leverage(exchange, trade["ticker"], trade["leverage"])
 
-            exchange.place_conditional_order(
+            exchange.place_order(
+                category="linear",
                 symbol=trade["ticker"],
-                order_type="Limit",
                 side="Buy" if trade["direction"] == "LONG" else "Sell",
-                qty=trade["position"],
-                price=trade["entry_price"],
-                base_price=round(trade["entry_price"] +
-                                 trade["move"], trade["price_scale"]),
-                stop_px=trade["entry_price"],
-                time_in_force="GoodTillCancel",
-                reduce_only=False,
-                close_on_trigger=False,
-                take_profit=trade["profit_target"],
-                stop_loss=trade["stop_loss"],
-                position_idx=0
+                orderType="Limit",
+                qty=str(trade["position"]),
+                price=str(trade["entry_price"]),
+                triggerPrice=str(trade["trigger_price"]),
+                triggerDirection= 2 if trade["direction"] == "LONG" else 1,
+                reduceOnly=False,
+                closeOnTrigger=False,
+                takeProfit=str(trade["profit_target"]),
+                stopLoss=str(trade["stop_loss"]),
+                positionIdx=0
             )
 
             trade_id = "{}_{}".format(trade["ticker"], trade["direction"])
@@ -111,7 +101,6 @@ def place_trades_on_exchange(trades: pd.DataFrame, exchange) -> pd.DataFrame:
             response.append({"trade_id": trade_id, "status": "ERROR"})
 
     return pd.DataFrame(response)
-
 
 def write_trades_to_audit_log(trades: pd.DataFrame, audit_log_path) -> None:
     trades["create_date"] = datetime.datetime.now()
@@ -125,34 +114,38 @@ def __parse_price_scale(instruments_info: dict, ticker: str) -> float:
                    for x in instruments_info_list if x["symbol"] == ticker]
     return int(price_scale[0]) if any(price_scale) else DEFAULT_PRICE_SCALE
 
-
 def __parse_qty_step(instruments_info: dict, ticker: str) -> str:
     instruments_info_list = instruments_info["result"]["list"]
     qty_step = [x["lotSizeFilter"]["qtyStep"]
                 for x in instruments_info_list if x["symbol"] == ticker]
     return qty_step[0] if any(qty_step) else DEFAULT_QTY_STEP
 
-
 def __set_leverage(exchange, ticker: str, leverage: float) -> None:
-    actual_leverage = exchange.my_position(
-        symbol=ticker)["result"][0]["leverage"]
+    response = exchange.get_positions(category="linear", symbol=ticker)
 
+    actual_leverage = float(response["result"]["list"][0]["leverage"])
     if leverage == actual_leverage:
         return
 
     exchange.set_leverage(
+        category="linear",
         symbol=ticker,
-        buy_leverage=leverage,
-        sell_leverage=leverage
+        buy_leverage=str(leverage),
+        sell_leverage=str(leverage)
     )
 
 
 def __set_isolate_margin(exchange, ticker: str, buy_leverage: float, sell_leverage: float) -> None:
-    is_isolated = exchange.my_position(
-        symbol=ticker)["result"][0]["is_isolated"]
-
+    response = exchange.get_positions(category="linear", symbol=ticker)
+    
+    is_isolated = response["result"]["list"][0]["tradeMode"] == 1
     if is_isolated:
         return
 
-    exchange.cross_isolated_margin_switch(
-        symbol=ticker, is_isolated=True, buy_leverage=buy_leverage, sell_leverage=sell_leverage)
+    exchange.switch_margin_mode(
+                category="linear",
+                symbol=ticker,
+                tradeMode=1,
+                buyLeverage=str(buy_leverage),
+                sellLeverage=str(sell_leverage)
+            )
